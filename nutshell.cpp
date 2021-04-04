de82923841817f1ef1f72716b9066898e6f5d0a1
juanfortoul <58518333+juanfortoul@users.noreply.github.com>
2021-04-03 20:57:49 -0400

    Add files via upload
    My nutshell

--- nutshell.cpp
+++ nutshell.cpp
@@ -4,6 +4,12 @@
 #include <limits.h>
 #include <pwd.h>
 #include "command.h"
+#include <cstring>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
@@ -15,6 +21,8 @@ extern FILE* yyin;
 extern void yyrestart(FILE*);
 std::vector<Command*> cmdTable;
 std::vector<char*> strTable;
+extern char ** environ;
+std::map<char*, char*> aliases;
 
 //
 void printPrompt() {
@@ -71,14 +79,192 @@ void testCommandTable() {
     }
 }
 
+int executeGeneral(char* path, char* params[]) {
+    int size = (sizeof(params)/sizeof(char*));
+    printf("%d\n", params[0]);
+
+    pid_t pid;
+
+    pid = fork();
+    if (pid < 0) {
+        printf("Error. Try again.\n");
+        return EXIT_FAILURE;
+    } else {
+        if (pid == 0) {
+            execv(path, params);
+            printf("Success\n");
+            return EXIT_SUCCESS;
+        } else {
+            wait(NULL);
+            EXIT_SUCCESS;
+        }
+    }
+}
+
+void shell_init() {
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    setenv("PATH", ".:/bin", 1);
+    setenv("HOME", cwd, 1);
+    setenv("PWD", cwd, 1);
+    /*
+    for(int i = 0; sizeof(environ), i++) {
+        if (environ[i].substr(0,4) == "PATH") {
+            environ[i] = "PATH=.:/bin";
+        }
+        if (environ[i].substr(0,4) == "HOME") {
+            environ[i] = "HOME=" + cwd;
+        }
+    }
+    */
+}
+
+void setEnv(char* var, char* word) {
+    setenv(var, word, 1);
+    /*
+    for(int i = 0; sizeof(environ), i++) {
+        if (environ[i].find(var) != std::string::npos) {
+            environ[i] = var + "=" + word;
+        }
+    }
+    */
+}
+
+void unsetEnv(char* var) {
+    unsetenv(var);
+}
+
+void printEnv() {
+    for(int i = 0; i < sizeof(environ); i++) {
+        printf("%s\n", environ[i]);
+    }
+}
+
+void checkGeneral(char* name, char* params[]) {
+    char* path = new char[strlen(name) + 5];
+    strcpy(path, "/bin");
+    strcat(path, "/");
+    strcat(path, name);
+    if (access(path, F_OK) == 0) {
+        int size = (sizeof(params)/sizeof(char*));
+        printf("%d\n", size);
+        char* newParams[size + 2];
+        newParams[0] = path;
+        for (int i = 0; i < size; i++) {
+            newParams[i + 1] = params[i];
+        }
+        newParams[size + 1] = NULL;
+        size = (sizeof(newParams)/sizeof(char*));
+        printf("%d\n", size);
+        executeGeneral(path, newParams);
+    };
+}
+
+void addAlias(char* key, char* params[]) {
+    char* command = (char*)"";
+    int size = (sizeof(params)/sizeof(char*));
+    for (int i = 1; i < size; i++) {
+        strcat(command, " ");
+        strcat(command, params[i]);
+    }
+    aliases[key] = command;
+}
+
+void removeAlias(char* key) {
+    aliases.erase(key);
+}
+
+void executeAlias(char* command) {
+    
+}
+
+void printAliases() {
+    std::map<char*,char*>::iterator iter;
+    for (iter = aliases.begin(); iter != aliases.end(); iter++) {
+        std::cout << iter->first << " = " << iter->second << std::endl;
+    }
+}
+
+void changeDir(char* file) {
+    char* currentDir = getenv("PWD");
+    if (file[0] != '/') {
+        strcat(currentDir, "/");
+        strcat(currentDir, file);
+        setenv("PWD", currentDir, 1);
+        if (chdir(getenv("PWD")) != 0) {
+            std::cout << "Directory not found" << std::endl;
+        }
+    } else {
+        if (chdir(file) != 0) {
+            std::cout << "Directory not found" << std::endl;
+        }
+    }
+}
+
+int processCommand() {
+    for (int i = 0; i < cmdTable.size(); i++) {
+        char* name = cmdTable[i]->name;
+        char* params[cmdTable[i]->parameters.size()];
+        for (int j = 0; j < cmdTable[i]->parameters.size(); j++) {
+            params[j] = cmdTable[i]->parameters[j];
+        }
+        printf("%d\n", sizeof(params)/sizeof(char*));
+        if (aliases.find(name) != aliases.end()) {
+            executeAlias(name);
+        } else {
+            if (strcmp(name, "setenv") == 0) {
+                setEnv(params[0], params[1]);
+                return 0;
+            } else if (strcmp(name, "unsetenv") == 0) {
+                unsetEnv(params[0]);
+                return 0;
+            } else if (strcmp(name, "printenv") == 0) {
+                printEnv(); return 0;
+            } else if (strcmp(name, "alias") == 0) {
+                if (cmdTable[i]->parameters.size() == 0) {
+                    printAliases();
+                    return 0;
+                } else {
+                    addAlias(params[1], params);
+                    return 0;
+                }
+            } else if (strcmp(name, "unalias") == 0) {
+                removeAlias(params[1]);
+                return 0;
+            } else if (strcmp(name, "cd") == 0) {
+                changeDir(params[0]);
+            } else if (strcmp(name, "bye") == 0) {
+                return 1;
+            } else {
+                checkGeneral(name, params);
+                return 0;
+            }
+        }
+    }
+}
+
 int main() {
     system("clear");
+    shell_init();
     while(1) {
         printPrompt();
-        if (yyparse() == 0) {
-            printf("success\n");
-        };
-        testCommandTable();
+        yyparse();
+        if (processCommand() == 1) {
+		    printf("Successfully exited shell\n");
+			break;
+		};
+        /*
+        switch(yyparse()) {
+			case 0: if (processCommand() == 1) {
+						printf("Successfully exited shell");
+						break;
+					};
+			case 1: fprintf(stderr, "%s\n", "Syntax Error");
+					break;
+			default: break;
+		}
+        */
+        //testCommandTable();
         yyrestart(yyin);
         cleanUp();
     }
